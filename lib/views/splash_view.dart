e230d9ae3aa6f130eb406a3d9d73054b05110d2b
Mahmoud Mohamed <mahmoudmohaamed635@gmail.com>
2026-06-04 09:56:52 +0300

    create Splash view body

---
+++ lib/views/splash_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:url_to_view/widgets/SplashViewBody.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Splashviewbody());
+  }
+}
